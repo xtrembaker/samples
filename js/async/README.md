f894150d5bee886fb0214fc51d71eef730349110
Arnaud Busson <arnaud.busson@inovia-team.com>
2016-10-21 13:56:39 +0200

    Async await

---
+++ js/async/README.md
@@ -0,0 +1,4 @@
+ASYNC / AWAIT :
+
+./node_modules/babel-cli/bin/babel.js async_await.js > test_async_await.js
+node  test_async_await.js
--- js/async/async_await.js
+++ js/async/async_await.js
@@ -6,23 +6,30 @@ var max = 10;
  *
  * @see https://babeljs.io/docs/plugins/preset-stage-2/
  * @returns {Promise}
- * @todo This script is not implemented yet, need to install babel preset to make async/await working
  */
 function fetch(){
     return new Promise(function(resolve, reject){
         setTimeout(function(){
-
+            resolve();
         }, 3000);
     })
 }
 
 
+/**
+ * /!\ CAN'T throw error in async / await : (node:23852) UnhandledPromiseRejectionWarning: Unhandled promise rejection (rejection id: 1): ##ERROR##
+ */
 async function trigger(){
-    await fetch().then(function(){
-        if(i < max){
-            trigger();
-        }
-    });
+    throw "erreur";
+    var result = await fetch();
+    console.log('i : ', i);
+    if(i < max){
+        i++;
+        trigger();
+    }
 }
 
 trigger();
+
+
+
