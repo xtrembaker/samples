4a171e01aef6ecd2070e88a68235d7f156dcc6b1
Arnaud Busson <abusson@civiliz.fr>
2018-09-02 16:36:12 +0200

    js/jest_unit_test

--- js/jest_unit_test/test.spec.js
+++ js/jest_unit_test/test.spec.js
@@ -1,12 +1,14 @@
 //import jest from 'jest';
 const sum = require('./sum');
 
+describe.only('sum test', () => {
 
-/*test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+    test.only('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+
+    test('adds 3 + 2 to equal 5', () => {
+        expect(sum(3, 2)).toBe(5);
+    });
+
 });
-
-test('adds 3 + 2 to equal 5', () => {
-  expect(sum(3, 2)).toBe(5);
-});*/
-
