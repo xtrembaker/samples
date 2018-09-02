4e0f64d3a03d65d038e31532e67a138eb79d5231
Arnaud Busson <abusson@civiliz.fr>
2018-09-02 16:37:06 +0200

    js/webpack2

---
+++ js/webpack2/webpack.config.js
@@ -0,0 +1,9 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpackConfig = {
+  entry: 'index.js',
+  output: {
+    path: 'dist',
+    filename: 'index_bundle.js'
+  },
+  plugins: [new HtmlWebpackPlugin()]
+};
